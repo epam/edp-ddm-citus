f586503d6e83c155cd79138b80bae76b8cb2048c
Anton Tupitsyn <anton_tupitsyn@epam.com>
2021-09-27 15:46:11 +0000

    [EDP-DDM-9373] Don't crash on hyphens in usernames

--- platform-db/changesets/registry/procedures/p_grant_analytics_user.sql
+++ platform-db/changesets/registry/procedures/p_grant_analytics_user.sql
@@ -19,10 +19,10 @@ BEGIN
     return;
   end if;
 
-  execute 'grant connect on database ' || current_database() || ' to ' || p_user_name || ';';
+  execute 'grant connect on database ' || current_database() || ' to "' || p_user_name || '";';
 
   FOR r IN SELECT * FROM information_schema.views WHERE table_name LIKE c_obj_pattern AND table_schema = 'public' LOOP
-    EXECUTE 'GRANT SELECT ON ' || r.table_name || ' TO ' || p_user_name || ';';
+    EXECUTE 'GRANT SELECT ON ' || r.table_name || ' TO "' || p_user_name || '";';
   END LOOP;
  END;
 $procedure$
