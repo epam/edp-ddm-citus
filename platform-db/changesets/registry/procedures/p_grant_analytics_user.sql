59851d2bd0763e3c2aca5139cac59dd0f339f92e
Oleksii Baranov <oleksii_baranov@epam.com>
2021-09-28 16:31:42 +0000

    [EDP-DDM-9373] Fix to p_grant_analytics_user

--- platform-db/changesets/registry/procedures/p_grant_analytics_user.sql
+++ platform-db/changesets/registry/procedures/p_grant_analytics_user.sql
@@ -7,22 +7,23 @@ DECLARE
   c_obj_pattern TEXT := 'report%v';
   r RECORD;
   is_role_found integer;
+  v_user_name text := replace(p_user_name, '"', '');
 BEGIN
 
   -- check if role exists
   select 1
     into is_role_found
     from pg_catalog.pg_roles
-    where rolname = p_user_name;
+    where rolname = v_user_name;
 
   if is_role_found is null then
-    return;
+    raise exception 'Role [%] is not found', v_user_name;
   end if;
 
-  execute 'grant connect on database ' || current_database() || ' to ' || p_user_name || ';';
+  execute 'grant connect on database ' || current_database() || ' to "' || v_user_name || '";';
 
   FOR r IN SELECT * FROM information_schema.views WHERE table_name LIKE c_obj_pattern AND table_schema = 'public' LOOP
-    EXECUTE 'GRANT SELECT ON ' || r.table_name || ' TO ' || p_user_name || ';';
+    EXECUTE 'GRANT SELECT ON "' || r.table_name || '" TO "' || v_user_name || '";';
   END LOOP;
  END;
 $procedure$
