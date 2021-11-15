88324fa5b769711eba08ac6aa5155b89ad980dce
Anton Tupitsyn <anton_tupitsyn@epam.com>
2021-11-15 13:31:41 +0000

    [EDP-DDM-10190] Add ddm_source tables to replica

--- platform-db/changesets/registry/procedures/f_get_tables_to_replicate.sql
+++ platform-db/changesets/registry/procedures/f_get_tables_to_replicate.sql
@@ -5,13 +5,23 @@ CREATE OR REPLACE FUNCTION f_get_tables_to_replicate(p_publication_name TEXT)
  LANGUAGE plpgsql
 AS $function$
 BEGIN
-  RETURN (SELECT string_agg(table_name,', ') FROM (
-            SELECT table_name FROM information_schema.tables
-            WHERE table_schema = 'registry' AND table_type = 'BASE TABLE'
-              AND (table_name NOT LIKE 'databasechangelog%' AND table_name NOT LIKE 'ddm%')
-            EXCEPT
-            SELECT tablename FROM pg_catalog.pg_publication_tables WHERE schemaname = 'registry' and pubname = p_publication_name
-          ) s
+  RETURN (SELECT string_agg(table_name, ', ')
+            FROM (
+                    SELECT table_name
+                    FROM information_schema.tables
+                    WHERE table_type = 'BASE TABLE'
+                        AND (
+                            table_schema = 'registry'
+                            OR (
+                                table_schema = 'public'
+                                and table_name like 'ddm_source%'
+                            )
+                        )
+                    EXCEPT
+                    SELECT tablename
+                    FROM pg_catalog.pg_publication_tables
+                    WHERE pubname = p_publication_name
+                ) s
          );
 END;
 $function$
