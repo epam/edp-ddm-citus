bf60ce78836e2b2f2a5e845a09653d6202e0e38b
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2022-06-07 11:29:35 +0000

    [EDP-DDM-13213] Added a custom citus image with the postgis extension

--- platform-db/changesets/postgres/00050_create-other-extensions.sql
+++ platform-db/changesets/postgres/00050_create-other-extensions.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset platform:create-other-extensions
--- extension pg_stat_statements
+-- extension pg_stat_statements and postgis
 create extension if not exists pg_stat_statements;
