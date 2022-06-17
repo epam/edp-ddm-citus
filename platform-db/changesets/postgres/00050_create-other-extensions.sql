48acbaea8c16f4ee88edd33f7f4f571a4cc4ac7c
Stanislav Kostenko <stanislav_kostenko@epam.com>
2022-06-17 12:46:00 +0300

    [EDP-DDM-13213] Revert liquibase script

--- platform-db/changesets/postgres/00050_create-other-extensions.sql
+++ platform-db/changesets/postgres/00050_create-other-extensions.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset platform:create-other-extensions
--- extension pg_stat_statements and postgis
+-- extension pg_stat_statements
 create extension if not exists pg_stat_statements;
